90533f566e0f49539db07856a1d4fb429bd99ec0
TimurGorshkov <gorshkoff.timur@gmail.com>
2019-03-17 00:25:20 +0300

    Кнопки

--- app/src/main/java/com/timmyg/quiz/timmygquiz/QuizActivity.java
+++ app/src/main/java/com/timmyg/quiz/timmygquiz/QuizActivity.java
@@ -2,12 +2,36 @@ package com.timmyg.quiz.timmygquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        trueButton=(Button)findViewById(R.id.true_button);
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        falseButton=(Button)findViewById(R.id.false_button);
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }
