eaaba7fbe568691077e6589f9cf4684083633f11
GorshkovTimur <gorshkoff.timur@gmail.com>
2019-07-28 15:07:06 +0300

    Запилил IdentityMap  в тот же класс

--- src/EmployeeMapper.java
+++ src/EmployeeMapper.java
@@ -1,21 +1,33 @@
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.zip.DataFormatException;
 
 public class EmployeeMapper {
 
     private JDBCEmulator emulator;
+    private Map<Integer,Employee> employeeMap;
 
     public EmployeeMapper(JDBCEmulator emulator) {
         this.emulator = emulator;
+        this.employeeMap = new HashMap<>();
+
     }
 
     public Employee findById(int id){
-        if (id>0){
-            Employee employee = new Employee(emulator.getNameFromDb(),emulator.getHeadFromDb());
-            return employee;
-        } else {
-            throw new InputMismatchException("Id должен быть больше 0");
-        }
+            checkId(id);
+            if (employeeMap.containsKey(id)) {
+                return employeeMap.get(id);
+            } else {
+                Employee employee = new Employee(emulator.getNameFromDb(), emulator.getHeadFromDb());
+                employeeMap.put(id, employee);
+                return employee;
+            }
+
+    }
+
+    private void checkId(int id) {
+        if (id<0) throw new InputMismatchException("Id must bee positive number");
     }
 
     public void delete(Employee employee){
